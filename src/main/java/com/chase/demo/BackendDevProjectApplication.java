0cb7834cd1874567c71529db86794ba959b7d188
Chase-developer <dymaxuschase@gmail.com>
2025-04-20 16:52:27 +0800

    v0.0.1
    Added Project

---
+++ src/main/java/com/chase/demo/BackendDevProjectApplication.java
@@ -0,0 +1,13 @@
+package com.chase.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDevProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDevProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chase/demo/BackendDevProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chase.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDevProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
